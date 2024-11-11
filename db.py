3bc13c5a130fd17e6c88c81685b23cfd501ef4e9
MJCappella <mjaymickey001@gmail.com>
2024-11-11 16:51:10 +0300

    edits: minor changes

--- db.py
+++ db.py
@@ -7,3 +7,22 @@ load_dotenv()
 
 ENDPOINT = os.getenv("ASTRA_ENDPOINT")
 TOKEN = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+
+# connecting to astra db
+def get_db():
+    client = DataAPIClient(TOKEN)
+    db = client.get_database_by_api_endpoint(ENDPOINT)
+    return db
+
+db =  get_db()
+collection_names = ["personal_data", "notes"]
+
+# creating collections in the db
+for collection in collection_names:
+    try:
+        db.create_collection(collection)
+    except:
+        pass
+    
+personal_data_collection = db.get_collection("personal_data")
+notes_collection = db.get_collection("notes")
---
+++ profiles.py
@@ -0,0 +1,6 @@
+from .db import personal_data_collection, notes_collection
+
+def get_values(_id):
+    return {
+        
+    }
