5ebbf25adaf0d90bea63023467e8d24a6d09ce6d
MJCappella <mjaymickey001@gmail.com>
2024-11-11 16:32:54 +0300

    created an astra db connection

---
+++ db.py
@@ -0,0 +1,9 @@
+from astrapy import DataAPIClient
+from dotenv import load_dotenv
+import streamlit as st
+import os
+
+load_dotenv()
+
+ENDPOINT = os.getenv("ASTRA_ENDPOINT")
+TOKEN = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
